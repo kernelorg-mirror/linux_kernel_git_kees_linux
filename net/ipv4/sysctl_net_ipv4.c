4835453ce464b3067d3ebb86e782bae273531381
Kees Cook <keescook@chromium.org>
2013-01-31 13:19:28 -0800

    tcp: sysctl to disable TCP simultaneous connect
    This is based on Willy Tarreau's patch from 2008[1]. The goal is to
    close a corner-case of TCP that isn't used and poses a small DoS risk.
    For systems that do not want to take any risk at all, this is a desirable
    configuration knob.
    
    It is possible for two clients to connect with crossed SYNs without
    checking sequence numbers. As such, it might be possible to guess a source
    port number to block a system from making connections to well-known
    ports and IP addresses (e.g. auto-update checks) without requiring a
    MiTM position.
    
    The feature can now be disabled via sysctl:
    
    $ echo 0 > /proc/sys/net/ipv4/tcp_simult_connect
    $ echo ohai | nc -w 1 -p 50000 localhost 50000 -v -v -v
    nc: connect to localhost port 50000 (tcp) timed out: Operation now in progress
    nc: connect to localhost port 50000 (tcp) timed out: Operation now in progress
    
    $ echo 1 > /proc/sys/net/ipv4/tcp_simult_connect
    $ echo ohai | nc -w 1 -p 50000 localhost 50000 -v -v -v
    Connection to localhost 50000 port [tcp/*] succeeded!
    ohai
    
    [1] http://thread.gmane.org/gmane.linux.network/107971
    
    Cc: Willy Tarreau <w@1wt.eu>
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- net/ipv4/sysctl_net_ipv4.c
+++ net/ipv4/sysctl_net_ipv4.c
@@ -581,6 +581,15 @@ static struct ctl_table ipv4_table[] = {
 		.extra2		= &four,
 	},
 	{
+		.procname	= "tcp_simult_connect",
+		.data		= &sysctl_tcp_simult_connect,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= &zero,
+		.extra2		= &one,
+	},
+	{
 		.procname	= "tcp_min_tso_segs",
 		.data		= &sysctl_tcp_min_tso_segs,
 		.maxlen		= sizeof(int),
