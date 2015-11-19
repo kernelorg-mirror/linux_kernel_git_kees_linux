70fef9204769be474ef787daf2c81b33be664cd3
Kees Cook <keescook@chromium.org>
2015-11-19 12:14:53 -0800

    fs: clear file privilege bits when mmap writing
    Normally, when a user can modify a file that has setuid or setgid bits,
    those bits are cleared when they are not the file owner or a member
    of the group. This is enforced when using write and truncate but not
    when writing to a shared mmap on the file. This could allow the file
    writer to gain privileges by changing a binary without losing the
    setuid/setgid/caps bits.
    
    Changing the bits requires holding inode->i_mutex, so it cannot be done
    during the page fault (due to mmap_sem being held during the fault).
    Instead, clear the bits if PROT_WRITE is being used at mmap open time,
    or added at mprotect time.
    
    Since we can't do the check in the right place inside mmap (due to
    holding mmap_sem), we have to do it before holding mmap_sem, which
    means duplicating some checks, which have to be available to the non-MMU
    builds too.
    
    When walking VMAs during mprotect, we need to drop mmap_sem (while
    holding a file reference) and restart the walk after clearing privileges.
    
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- mm/mprotect.c
+++ mm/mprotect.c
@@ -12,6 +12,7 @@
 #include <linux/hugetlb.h>
 #include <linux/shm.h>
 #include <linux/mman.h>
+#include <linux/file.h>
 #include <linux/fs.h>
 #include <linux/highmem.h>
 #include <linux/security.h>
@@ -382,6 +383,7 @@ SYSCALL_DEFINE3(mprotect, unsigned long, start, size_t, len,
 
 	vm_flags = calc_vm_prot_bits(prot);
 
+restart:
 	down_write(&current->mm->mmap_sem);
 
 	vma = find_vma(current->mm, start);
@@ -421,6 +423,28 @@ SYSCALL_DEFINE3(mprotect, unsigned long, start, size_t, len,
 		if ((newflags & ~(newflags >> 4)) & (VM_READ | VM_WRITE | VM_EXEC)) {
 			error = -EACCES;
 			goto out;
+		}
+
+		/*
+		 * If we're adding write permissions to a shared file,
+		 * we must clear privileges (like done at mmap time),
+		 * but we have to juggle the locks to avoid holding
+		 * mmap_sem while holding i_mutex.
+		 */
+		if ((vma->vm_flags & VM_SHARED) && vma->vm_file &&
+		    (newflags & VM_WRITE) && !(vma->vm_flags & VM_WRITE) &&
+		    file_needs_remove_privs(vma->vm_file)) {
+			struct file *file = get_file(vma->vm_file);
+
+			start = vma->vm_start;
+			up_write(&current->mm->mmap_sem);
+			mutex_lock(&file_inode(file)->i_mutex);
+			error = file_remove_privs(file);
+			mutex_unlock(&file_inode(file)->i_mutex);
+			fput(file);
+			if (error)
+				return error;
+			goto restart;
 		}
 
 		error = security_file_mprotect(vma, reqprot, prot);
