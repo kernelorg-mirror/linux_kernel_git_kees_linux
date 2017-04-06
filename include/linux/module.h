8a608bd950fb9c17eeb80cf3a9178a59d9d8cce5
Eddie Kovsky <ewk@edkovsky.org>
2017-04-05 21:35:49 -0600

    module: verify address is read-only
    Implement a mechanism to check if a module's address is in
    the rodata or ro_after_init sections. It mimics the existing functions
    that test if an address is inside a module's text section.
    
    Functions that take a module as an argument will be able to verify that the
    module address is in a read-only section. The idea is to prevent structures
    (including modules) that are not read-only from being passed to functions.
    
    This implements the first half of a suggestion made by Kees Cook for
    the Kernel Self Protection Project:
    
        - provide mechanism to check for ro_after_init memory areas, and
          reject structures not marked ro_after_init in vmbus_register()
    
    Suggested-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Eddie Kovsky <ewk@edkovsky.org>
    Acked-by: Jessica Yu <jeyu@redhat.com>
    Acked-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Link: https://lore.kernel.org/r/20170406033550.32525-2-ewk@edkovsky.org

--- include/linux/module.h
+++ include/linux/module.h
@@ -568,7 +568,9 @@ static inline bool module_is_live(struct module *mod)
 
 struct module *__module_text_address(unsigned long addr);
 struct module *__module_address(unsigned long addr);
+struct module *__module_rodata_address(unsigned long addr);
 bool is_module_address(unsigned long addr);
+bool is_module_rodata_address(unsigned long addr);
 bool __is_module_percpu_address(unsigned long addr, unsigned long *can_addr);
 bool is_module_percpu_address(unsigned long addr);
 bool is_module_text_address(unsigned long addr);
@@ -693,12 +695,22 @@ static inline struct module *__module_address(unsigned long addr)
 	return NULL;
 }
 
+static inline struct module *__module_rodata_address(unsigned long addr)
+{
+	return NULL;
+}
+
 static inline struct module *__module_text_address(unsigned long addr)
 {
 	return NULL;
 }
 
 static inline bool is_module_address(unsigned long addr)
+{
+	return false;
+}
+
+static inline bool is_module_rodata_address(unsigned long addr)
 {
 	return false;
 }
