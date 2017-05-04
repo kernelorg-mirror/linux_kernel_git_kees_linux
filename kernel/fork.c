0a5102b18808904e151ab8e8b5d812c181cf2349
Richard Fellner <richard.fellner@student.tugraz.at>
2017-05-04 14:26:50 +0200

    KAISER: Kernel Address Isolation
    This patch introduces our implementation of KAISER (Kernel Address Isolation to
    have Side-channels Efficiently Removed), a kernel isolation technique to close
    hardware side channels on kernel address information.
    
    More information about the patch can be found on:
    
            https://github.com/IAIK/KAISER
    
    From: Richard Fellner <richard.fellner@student.tugraz.at>
    From: Daniel Gruss <daniel.gruss@iaik.tugraz.at>
    Subject: [RFC, PATCH] x86_64: KAISER - do not map kernel in user mode
    Date: Thu, 4 May 2017 14:26:50 +0200
    Link: http://marc.info/?l=linux-kernel&m=149390087310405&w=2
    Kaiser-4.10-SHA1: c4b1831d44c6144d3762ccc72f0c4e71a0c713e5
    
    To: <linux-kernel@vger.kernel.org>
    To: <kernel-hardening@lists.openwall.com>
    Cc: <clementine.maurice@iaik.tugraz.at>
    Cc: <moritz.lipp@iaik.tugraz.at>
    Cc: Michael Schwarz <michael.schwarz@iaik.tugraz.at>
    Cc: Richard Fellner <richard.fellner@student.tugraz.at>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: <kirill.shutemov@linux.intel.com>
    Cc: <anders.fogh@gdata-adan.de>
    
    After several recent works [1,2,3] KASLR on x86_64 was basically
    considered dead by many researchers. We have been working on an
    efficient but effective fix for this problem and found that not mapping
    the kernel space when running in user mode is the solution to this
    problem [4] (the corresponding paper [5] will be presented at ESSoS17).
    
    With this RFC patch we allow anybody to configure their kernel with the
    flag CONFIG_KAISER to add our defense mechanism.
    
    If there are any questions we would love to answer them.
    We also appreciate any comments!
    
    Cheers,
    Daniel (+ the KAISER team from Graz University of Technology)
    
    [1] http://www.ieee-security.org/TC/SP2013/papers/4977a191.pdf
    [2] https://www.blackhat.com/docs/us-16/materials/us-16-Fogh-Using-Undocumented-CPU-Behaviour-To-See-Into-Kernel-Mode-And-Break-KASLR-In-The-Process.pdf
    [3] https://www.blackhat.com/docs/us-16/materials/us-16-Jang-Breaking-Kernel-Address-Space-Layout-Randomization-KASLR-With-Intel-TSX.pdf
    [4] https://github.com/IAIK/KAISER
    [5] https://gruss.cc/files/kaiser.pdf
    
    (cherry picked from Change-Id: I0eb000c33290af01fc4454ca0c701d00f1d30b1d)
    
    Conflicts:
    arch/x86/entry/entry_64.S (not in this tree)
    arch/x86/kernel/entry_64.S (patched instead of that)
    arch/x86/entry/entry_64_compat.S (not in this tree)
    arch/x86/ia32/ia32entry.S (patched instead of that)
    arch/x86/include/asm/hw_irq.h
    arch/x86/include/asm/pgtable_types.h
    arch/x86/include/asm/processor.h
    arch/x86/kernel/irqinit.c
    arch/x86/kernel/process.c
    arch/x86/mm/Makefile
    arch/x86/mm/pgtable.c
    init/main.c
    
    Signed-off-by: Hugh Dickins <hughd@google.com>
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- kernel/fork.c
+++ kernel/fork.c
@@ -155,8 +155,12 @@ static struct thread_info *alloc_thread_info_node(struct task_struct *tsk,
 	return page ? page_address(page) : NULL;
 }
 
+extern void kaiser_remove_mapping(unsigned long start_addr, unsigned long size);
 static inline void free_thread_info(struct thread_info *ti)
 {
+#ifdef CONFIG_KAISER
+	kaiser_remove_mapping((unsigned long)ti, THREAD_SIZE);
+#endif
 	free_memcg_kmem_pages((unsigned long)ti, THREAD_SIZE_ORDER);
 }
 # else
@@ -292,6 +296,7 @@ int __attribute__((weak)) arch_dup_task_struct(struct task_struct *dst,
 	return 0;
 }
 
+extern void kaiser_add_mapping(unsigned long addr, unsigned long size, unsigned long flags);
 static struct task_struct *dup_task_struct(struct task_struct *orig)
 {
 	struct task_struct *tsk;
@@ -313,6 +318,9 @@ static struct task_struct *dup_task_struct(struct task_struct *orig)
 		goto free_ti;
 
 	tsk->stack = ti;
+#ifdef CONFIG_KAISER
+	kaiser_add_mapping((unsigned long)tsk->stack, THREAD_SIZE, __PAGE_KERNEL);
+#endif
 
 	setup_thread_stack(tsk, orig);
 	clear_user_return_notifier(tsk);
