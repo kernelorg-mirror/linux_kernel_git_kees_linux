eafd63ea2329ee39e5eec5dfa9f8e06c181cb4d6
Thomas Garnier <thgarnie@google.com>
2017-09-20 10:43:33 -0700

    x86/mm: Make the x86 GOT read-only
    The GOT is changed during early boot when relocations are applied. Make
    it read-only directly. This table exists only for PIE binary.
    
    Position Independent Executable (PIE) support will allow to extended the
    KASLR randomization range below the -2G memory limit.
    
    Signed-off-by: Thomas Garnier <thgarnie@google.com>
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- include/asm-generic/vmlinux.lds.h
+++ include/asm-generic/vmlinux.lds.h
@@ -282,6 +282,17 @@
 	VMLINUX_SYMBOL(__end_ro_after_init) = .;
 #endif
 
+#ifdef CONFIG_X86_PIE
+#define RO_GOT_X86							\
+	.got        : AT(ADDR(.got) - LOAD_OFFSET) {			\
+		VMLINUX_SYMBOL(__start_got) = .;			\
+		*(.got);						\
+		VMLINUX_SYMBOL(__end_got) = .;				\
+	}
+#else
+#define RO_GOT_X86
+#endif
+
 /*
  * Read only Data
  */
@@ -338,6 +349,7 @@
 		VMLINUX_SYMBOL(__end_builtin_fw) = .;			\
 	}								\
 									\
+	RO_GOT_X86							\
 	TRACEDATA							\
 									\
 	/* Kernel symbol table: Normal symbols */			\
