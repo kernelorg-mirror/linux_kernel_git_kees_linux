fdbff58c82d0277c006bd43cdebd7fcf678b14e5
Andrea Arcangeli <aarcange@redhat.com>
2017-12-05 21:14:48 +0100

    x86/mm/kaiser: remove scratch registers
    This allows to flip CR3 without the need of functional kernel stack,
    which in turns allow to remove from the per-cpu user map some
    structures and simplifies the code significantly.
    
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- arch/x86/kernel/kaiser.h
+++ arch/x86/kernel/kaiser.h
@@ -13,51 +13,68 @@
 	orq	$(KAISER_SWITCH_MASK), \reg
 .endm
 
-.macro SWITCH_TO_KERNEL_CR3 scratch_reg:req
-	mov	%cr3, \scratch_reg
-	ADJUST_KERNEL_CR3 \scratch_reg
-	mov	\scratch_reg, %cr3
+.macro SWITCH_TO_KERNEL_CR3
+	testl   $1, PER_CPU_VAR(kaiser_enabled_pcp)
+	jz      .Lnokaiser_\@
+
+	movq    %rax, PER_CPU_VAR(kaiser_scratch)
+	movq	%cr3, %rax
+	ADJUST_KERNEL_CR3 %rax
+	movq	%rax, %cr3
+	movq	PER_CPU_VAR(kaiser_scratch), %rax
+.Lnokaiser_\@:
 .endm
 
-.macro SWITCH_TO_USER_CR3 scratch_reg:req
-	mov	%cr3, \scratch_reg
-	ADJUST_USER_CR3 \scratch_reg
-	mov	\scratch_reg, %cr3
+.macro SWITCH_TO_USER_CR3
+	testl   $1, PER_CPU_VAR(kaiser_enabled_pcp)
+	jz      .Lnokaiser_\@
+
+	movq    %rax, PER_CPU_VAR(kaiser_scratch)
+	movq	%cr3, %rax
+	ADJUST_USER_CR3 %rax
+	movq	%rax, %cr3
+	movq	PER_CPU_VAR(kaiser_scratch), %rax
+.Lnokaiser_\@:
 .endm
 
 .macro SAVE_AND_SWITCH_TO_KERNEL_CR3 scratch_reg:req save_reg:req
-	movq	%cr3, %r\scratch_reg
-	movq	%r\scratch_reg, \save_reg
+	testl   $1, PER_CPU_VAR(kaiser_enabled_pcp)
+	jz      .Lnokaiser_\@
+
+	movq	%cr3, \scratch_reg
+	movq	\scratch_reg, \save_reg
 	/*
 	 * Is the switch bit zero?  This means the address is
 	 * up in real KAISER patches in a moment.
 	 */
-	testq	$(KAISER_SWITCH_MASK), %r\scratch_reg
-	jz	.Ldone_\@
+	testq	$(KAISER_SWITCH_MASK), \scratch_reg
+	jz	.Lnokaiser_\@
 
-	ADJUST_KERNEL_CR3 %r\scratch_reg
-	movq	%r\scratch_reg, %cr3
-
-.Ldone_\@:
+	ADJUST_KERNEL_CR3 \scratch_reg
+	movq	\scratch_reg, %cr3
+.Lnokaiser_\@:
 .endm
 
-.macro RESTORE_CR3 save_reg:req
-	/*
-	 * The CR3 write could be avoided when not changing its value,
-	 * but would require a CR3 read *and* a scratch register.
-	 */
+.macro RESTORE_CR3 scratch_reg:req save_reg:req
+	testl   $1, PER_CPU_VAR(kaiser_enabled_pcp)
+	jz      .Lnokaiser_\@
+
+	movq	%cr3, \scratch_reg
+	testq \save_reg, \scratch_reg
+	je .Lnokaiser_\@
 	movq	\save_reg, %cr3
+.Lnokaiser_\@:
 .endm
 
 #else /* CONFIG_KAISER=n: */
 
-.macro SWITCH_TO_KERNEL_CR3 scratch_reg:req
+.macro SWITCH_TO_KERNEL_CR3
 .endm
-.macro SWITCH_TO_USER_CR3 scratch_reg:req
+.macro SWITCH_TO_USER_CR3
 .endm
 .macro SAVE_AND_SWITCH_TO_KERNEL_CR3 scratch_reg:req save_reg:req
 .endm
-.macro RESTORE_CR3 save_reg:req
+.macro RESTORE_CR3 scratch_reg:req save_reg:req
 .endm
 
 #endif
