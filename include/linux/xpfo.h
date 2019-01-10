a53eaa0581bc2c985c4f3711d8146a4f76a6d5a8
Tycho Andersen <tycho@docker.com>
2019-01-10 14:09:41 -0700

    mm: add a user_virt_to_phys symbol
    We need someting like this for testing XPFO. Since it's architecture
    specific, putting it in the test code is slightly awkward, so let's make it
    an arch-specific symbol and export it for use in LKDTM.
    
    v6: * add a definition of user_virt_to_phys in the !CONFIG_XPFO case
    
    CC: linux-arm-kernel@lists.infradead.org
    CC: x86@kernel.org
    Signed-off-by: Tycho Andersen <tycho@docker.com>
    Tested-by: Marco Benatto <marco.antonio.780@gmail.com>
    Signed-off-by: Khalid Aziz <khalid.aziz@oracle.com>
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- include/linux/xpfo.h
+++ include/linux/xpfo.h
@@ -23,6 +23,10 @@ struct page;
 
 #ifdef CONFIG_XPFO
 
+#include <linux/dma-mapping.h>
+
+#include <linux/types.h>
+
 extern struct page_ext_operations page_xpfo_ops;
 
 void set_kpte(void *kaddr, struct page *page, pgprot_t prot);
@@ -48,6 +52,8 @@ void xpfo_temp_unmap(const void *addr, size_t size, void **mapping,
 
 bool xpfo_enabled(void);
 
+phys_addr_t user_virt_to_phys(unsigned long addr);
+
 #else /* !CONFIG_XPFO */
 
 static inline void xpfo_kmap(void *kaddr, struct page *page) { }
@@ -71,6 +77,8 @@ static inline void xpfo_temp_unmap(const void *addr, size_t size,
 
 
 static inline bool xpfo_enabled(void) { return false; }
+
+static inline phys_addr_t user_virt_to_phys(unsigned long addr) { return 0; }
 
 #endif /* CONFIG_XPFO */
 
