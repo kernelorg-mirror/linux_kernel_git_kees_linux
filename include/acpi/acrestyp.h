e66b3a302a2c1ae26dcf3313f423d567c6ecb083
Kees Cook <keescook@chromium.org>
2022-07-22 16:00:01 -0700

    ACPICA: Fix UBSAN_BOUNDS failure for fake flexible array
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- include/acpi/acrestyp.h
+++ include/acpi/acrestyp.h
@@ -679,7 +679,10 @@ struct acpi_pci_routing_table {
 	u32 pin;
 	u64 address;		/* here for 64-bit alignment */
 	u32 source_index;
-	char source[4];		/* pad to 64 bits so sizeof() works in all cases */
+	union {
+		char __pad[4];	/* pad to 64 bits so sizeof() works in all cases */
+		DECLARE_FLEX_ARRAY(char, source);
+	};
 };
 
 #endif				/* __ACRESTYP_H__ */
