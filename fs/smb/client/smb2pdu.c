747a35e097f72391e2ceed3a99f77c61b5fd582b
Kees Cook <keescook@chromium.org>
2024-01-17 16:40:03 -0800

    cifs: Refactor intentional wrap-around test
    In an effort to separate intentional arithmetic wrap-around from
    unexpected wrap-around, we need to refactor places that depend on this
    kind of math. One of the most common code patterns of this is:
    
    	VAR + value < VAR
    
    Notably, this is considered "undefined behavior" for signed and pointer
    types, which the kernel works around by using the -fno-strict-overflow
    option in the build[1] (which used to just be -fwrapv). Regardless, we
    want to get the kernel source to the position where we can meaningfully
    instrument arithmetic wrap-around conditions and catch them when they
    are unexpected, regardless of whether they are signed[2], unsigned[3],
    or pointer[4] types.
    
    Refactor open-coded wrap-around addition test to use add_would_overflow().
    This paves the way to enabling the wrap-around sanitizers in the future.
    
    Link: https://git.kernel.org/linus/68df3755e383e6fecf2354a67b08f92f18536594 [1]
    Link: https://github.com/KSPP/linux/issues/26 [2]
    Link: https://github.com/KSPP/linux/issues/27 [3]
    Link: https://github.com/KSPP/linux/issues/344 [4]
    Cc: Steve French <sfrench@samba.org>
    Cc: Paulo Alcantara <pc@manguebit.com>
    Cc: Ronnie Sahlberg <lsahlber@redhat.com>
    Cc: Shyam Prasad N <sprasad@microsoft.com>
    Cc: Tom Talpey <tom@talpey.com>
    Cc: linux-cifs@vger.kernel.org
    Cc: samba-technical@lists.samba.org
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- fs/smb/client/smb2pdu.c
+++ fs/smb/client/smb2pdu.c
@@ -5007,7 +5007,7 @@ num_entries(int infotype, char *bufstart, char *end_of_buf, char **lastentry,
 	entryptr = bufstart;
 
 	while (1) {
-		if (entryptr + next_offset < entryptr ||
+		if (add_would_overflow(entryptr, next_offset) ||
 		    entryptr + next_offset > end_of_buf ||
 		    entryptr + next_offset + size > end_of_buf) {
 			cifs_dbg(VFS, "malformed search entry would overflow\n");
@@ -5023,7 +5023,7 @@ num_entries(int infotype, char *bufstart, char *end_of_buf, char **lastentry,
 			len = le32_to_cpu(dir_info->FileNameLength);
 
 		if (len < 0 ||
-		    entryptr + len < entryptr ||
+		    add_would_overflow(entryptr, len) ||
 		    entryptr + len > end_of_buf ||
 		    entryptr + len + size > end_of_buf) {
 			cifs_dbg(VFS, "directory entry name would overflow frame end of buf %p\n",
