3b8d758928e1ccbde5f1f5b1967dcfef9d598de6
Kees Cook <keescook@chromium.org>
2024-01-18 16:05:52 -0800

    overflow: Introduce add_wrap() and sub_wrap()
    Provide helpers that will perform wrapping addition or subtraction without
    tripping the arithmetic wrap-around sanitizers. The first argument is
    the type under which the wrap-around should happen with. In other words,
    these two calls will get very different results:
    
    	add_wrap(int, 50, 50) == 2500
    	add_wrap(u8,  50, 50) ==  196
    
    Cc: Rasmus Villemoes <rasmus.villemoes@prevas.dk>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: "Gustavo A. R. Silva" <gustavoars@kernel.org>
    Cc: linux-hardening@vger.kernel.org
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- include/linux/overflow.h
+++ include/linux/overflow.h
@@ -125,6 +125,24 @@ static inline bool __must_check __must_check_overflow(bool overflow)
 	}))
 
 /**
+ * add_wrap() - Intentionally perform a wrapping addition
+ * @type: type to check overflow against
+ * @a: first addend
+ * @b: second addend
+ *
+ * Return the potentially wrapped-around addition without
+ * tripping any overflow sanitizers that may be enabled.
+ */
+#define add_wrap(type, a, b)				\
+	({						\
+		type __sum;				\
+		if (check_add_overflow(a, b, &__sum)) {	\
+			/* do nothing */		\
+		}					\
+		__sum;					\
+	})
+
+/**
  * check_sub_overflow() - Calculate subtraction with overflow checking
  * @a: minuend; value to subtract from
  * @b: subtrahend; value to subtract from @a
@@ -138,6 +156,24 @@ static inline bool __must_check __must_check_overflow(bool overflow)
  */
 #define check_sub_overflow(a, b, d)	\
 	__must_check_overflow(__builtin_sub_overflow(a, b, d))
+
+/**
+ * sub_wrap() - Intentionally perform a wrapping subtraction
+ * @type: type to check underflow against
+ * @a: minuend; value to subtract from
+ * @b: subtrahend; value to subtract from @a
+ *
+ * Return the potentially wrapped-around subtraction without
+ * tripping any overflow sanitizers that may be enabled.
+ */
+#define sub_wrap(type, a, b)				\
+	({						\
+		type __val;				\
+		if (check_sub_overflow(a, b, &__val)) {	\
+			/* do nothing */		\
+		}					\
+		__val;					\
+	})
 
 /**
  * check_mul_overflow() - Calculate multiplication with overflow checking
