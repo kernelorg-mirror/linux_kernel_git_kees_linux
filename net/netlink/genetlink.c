9560e51ad4990b4d531808cd8cf8a118f9050fb9
Kees Cook <keescook@chromium.org>
2024-01-27 08:51:12 -0800

    net/netlink/af_netlink.c: underflow bug?: report
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- net/netlink/genetlink.c
+++ net/netlink/genetlink.c
@@ -1826,7 +1826,7 @@ static int genl_bind(struct net *net, int group)
 		if (family->n_mcgrps == 0)
 			continue;
 
-		i = group - family->mcgrp_offset;
+		i = wrapping_sub(int, group, family->mcgrp_offset);
 		if (i < 0 || i >= family->n_mcgrps)
 			continue;
 
