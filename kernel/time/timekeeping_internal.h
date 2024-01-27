1266c9d3f4cd8c540b79f6f0e89b3f41aa466099
Kees Cook <keescook@chromium.org>
2024-01-27 10:01:58 -0800

    [WIP] kernel/time/timekeeping_internal.h: wrapping math: use __unsigned_wrap
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- kernel/time/timekeeping_internal.h
+++ kernel/time/timekeeping_internal.h
@@ -16,7 +16,7 @@ extern void tk_debug_account_sleep_time(const struct timespec64 *t);
 #endif
 
 #ifdef CONFIG_CLOCKSOURCE_VALIDATE_LAST_CYCLE
-static inline u64 clocksource_delta(u64 now, u64 last, u64 mask)
+static inline __unsigned_wrap u64 clocksource_delta(u64 now, u64 last, u64 mask)
 {
 	u64 ret = (now - last) & mask;
 
@@ -27,7 +27,7 @@ static inline u64 clocksource_delta(u64 now, u64 last, u64 mask)
 	return ret & ~(mask >> 1) ? 0 : ret;
 }
 #else
-static inline u64 clocksource_delta(u64 now, u64 last, u64 mask)
+static inline __unsigned_wrap u64 clocksource_delta(u64 now, u64 last, u64 mask)
 {
 	return (now - last) & mask;
 }
