05f2b06292e0b90e64c81550bb5d2b5ea42d4931
Kees Cook <keescook@chromium.org>
2024-01-27 19:36:26 -0800

    [WIP] include/linux/skbuff.h: wrapping math bug?: use add_wrap()
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -3003,7 +3003,7 @@ static inline int skb_inner_network_offset(const struct sk_buff *skb)
 
 static inline int pskb_network_may_pull(struct sk_buff *skb, unsigned int len)
 {
-	return pskb_may_pull(skb, skb_network_offset(skb) + len);
+	return pskb_may_pull(skb, add_wrap(unsigned int, skb_network_offset(skb), len));
 }
 
 /*
