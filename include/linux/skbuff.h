708cdb5253edcf555c835ec083e824d38b151e30
Kees Cook <keescook@chromium.org>
2024-01-27 17:56:32 -0800

    include/linux/skbuff.h: negative offsets: use wrapping_sub()
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -3033,7 +3033,7 @@ static inline void skb_mac_header_rebuild(struct sk_buff *skb)
 
 static inline int skb_checksum_start_offset(const struct sk_buff *skb)
 {
-	return skb->csum_start - skb_headroom(skb);
+	return wrapping_sub(int, skb->csum_start, skb_headroom(skb));
 }
 
 static inline unsigned char *skb_checksum_start(const struct sk_buff *skb)
