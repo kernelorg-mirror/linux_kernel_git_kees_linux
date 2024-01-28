b680fb5e5d6f56d2ad2cd1d727ea33434b3b1d72
Kees Cook <keescook@chromium.org>
2024-01-28 12:33:10 -0800

    [WIP] kernel/fork.c: wrapping math counter: use inc_wrap()
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- kernel/fork.c
+++ kernel/fork.c
@@ -158,7 +158,7 @@ int nr_processes(void)
 	int total = 0;
 
 	for_each_possible_cpu(cpu)
-		total += per_cpu(process_counts, cpu);
+		inc_wrap(total, per_cpu(process_counts, cpu));
 
 	return total;
 }
