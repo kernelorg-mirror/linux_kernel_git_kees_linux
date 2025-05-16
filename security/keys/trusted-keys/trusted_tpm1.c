4d31d689255c9496ea4b71735707bf51373d7079
Kees Cook <kees@kernel.org>
2025-05-16 11:46:47 -0700

    security: keys: trusted: Explicitly overlap struct sdesc and struct shash_desc
    ...
    
    Signed-off-by: Kees Cook <kees@kernel.org>
    ---
    Cc: James Bottomley <James.Bottomley@HansenPartnership.com>
    Cc: Jarkko Sakkinen <jarkko@kernel.org>
    Cc: Mimi Zohar <zohar@linux.ibm.com>
    Cc: David Howells <dhowells@redhat.com>
    Cc: Paul Moore <paul@paul-moore.com>
    Cc: James Morris <jmorris@namei.org>
    Cc: "Serge E. Hallyn" <serge@hallyn.com>
    Cc: <linux-integrity@vger.kernel.org>
    Cc: <keyrings@vger.kernel.org>
    Cc: <linux-security-module@vger.kernel.org>

--- security/keys/trusted-keys/trusted_tpm1.c
+++ security/keys/trusted-keys/trusted_tpm1.c
@@ -28,9 +28,12 @@ static struct tpm_chip *chip;
 static struct tpm_digest *digests;
 
 struct sdesc {
-	struct shash_desc shash;
-	char ctx[];
+	TRAILING_OVERLAP(
+		struct shash_desc, shash, __ctx,
+		char ctx[];
+	);
 };
+TRAILING_OVERLAP_ASSERT(struct sdesc, shash.__ctx, ctx);
 
 static struct crypto_shash *hashalg;
 static struct crypto_shash *hmacalg;
